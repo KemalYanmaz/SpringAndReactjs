d3e69cedc1833559ea12b0e0f02bbe33e42e4456
KemalYanmaz <ulaskemalyanmaz@gmail.com>
2021-10-01 16:47:51 +0300

    database changed from mysql to h2

--- portfolio-work/src/main/java/com/kemalyanmaz/portfoliowork/config/SecurityConfig.java
+++ portfolio-work/src/main/java/com/kemalyanmaz/portfoliowork/config/SecurityConfig.java
@@ -14,11 +14,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/","/api").permitAll()
+		http.authorizeRequests().antMatchers("/","/api","/h2-console/**").permitAll()
 		.anyRequest().authenticated().and()
 		.formLogin()
 		.loginPage("/login").permitAll().and()
 		.logout().permitAll();
+		
+		http.csrf().disable();
+        http.headers().frameOptions().disable();
 	}
 
 	@Autowired
